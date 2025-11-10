2ab6fbde74b469c7f9e0843b430341796ef53e7d
Giovanni Sugamiele <gionnymiele@gmail.com>
2025-11-10 15:05:30 +0100

    aggiornamento immagini dinamiche

--- src/app/components/ResponsiveImage.js
+++ src/app/components/ResponsiveImage.js
@@ -1,6 +1,6 @@
 export default function ResponsiveImage({
   srcBase,
-  sizes = [400, 800, 1600],
+  sizes = [200, 400, 800, 1600],
   ext = "webp",
   alt = "",
   className = "",
@@ -8,7 +8,6 @@ export default function ResponsiveImage({
   // Se srcBase è un oggetto import Next.js, usa src
   const base = "images/resized";
   const filename = srcBase;
-  
 
   const srcSet = sizes
     .map((size) => `${base}/${size}/${filename}.${ext} ${size}w`)
@@ -18,7 +17,7 @@ export default function ResponsiveImage({
     <img
       src={`${base}/${sizes[0]}/${filename}.${ext}`} // fallback: dimensione più piccola
       srcSet={srcSet}
-      sizes="(max-width: 400px) 400px, (max-width: 800px) 800px, 1600px"
+      sizes="((max-width: 200px) 200px, max-width: 400px) 400px, (max-width: 800px) 800px, 1600px"
       alt={alt}
       className={className}
       loading="lazy"
