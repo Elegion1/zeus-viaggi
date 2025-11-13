122d1f4226dfde511f62ab621af9d056128c0240
Giovanni Sugamiele <gionnymiele@gmail.com>
2025-11-13 09:46:00 +0100

    gtm

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,11 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./globals.css"; // i tuoi stili personali
+import { GoogleTagManager } from "@next/third-parties/google";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="it">
+      <GoogleTagManager gtmId="GTM-M4KKC2H8" />
       <body>{children}</body>
     </html>
   );
